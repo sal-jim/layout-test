8ea3541fbed473e7ecb08b42f22288928e558bd6
Sal Jim <saljimeely@chai-haleeb.com>
2018-04-05 11:47:48 +0400

    first commit, testing purposes

---
+++ app/views/layouts/_layout.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! layout, :id, :title, :body, :created_at, :updated_at
+json.url layout_url(layout, format: :json)
---
+++ test/system/layouts_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class LayoutsTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit layouts_url
+  #
+  #   assert_selector "h1", text: "Layout"
+  # end
+end
